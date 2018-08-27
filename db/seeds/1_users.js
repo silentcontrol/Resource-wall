48dd6af94fccb862596859f829f81af57058c0a9
Chris Wang <chriswangscm@gmail.com>
2018-08-27 06:22:22 +0000

    updated seed file

--- db/seeds/1_users.js
+++ db/seeds/1_users.js
@@ -3,24 +3,24 @@ exports.seed = function(knex, Promise) {
   return Promise.all([
     knex('users').insert({
       id: 1,
-      username: 'Alice',
+      username: 'fakeDonaldTrump',
       password: 'asdf',
-      email: 'alice@email.com',
-      bio: 'Hello, my name is Alice!'
+      email: 'potus@whitehouse.gov',
+      bio: 'climate change is a hoax'
     }),
     knex('users').insert({
       id: 2,
-      username: 'Bob',
+      username: 'Quagmire',
       password: 'asdf',
-      email: 'bob@email.com',
-      bio: 'Hello, my name is Bob!'
+      email: 'glen@familyguy.com',
+      bio: 'giggity giggity giggity'
     }),
     knex('users').insert({
       id: 3,
-      username: 'Charlie',
+      username: 'Darthvader',
       password: 'asdf',
-      email: 'charlie@email.com',
-      bio: 'Hello, my name is Charlie!'
+      email: 'anakin@starwars.com',
+      bio: 'may the force be with you'
     }),
     knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 4')
   ]);
--- db/seeds/3_resources.js
+++ db/seeds/3_resources.js
@@ -3,39 +3,39 @@ exports.seed = function(knex, Promise) {
   return Promise.all([
     knex('resources').insert({
       id: 1,
-      url: 'http://www.wikipedia.com',
-      title: 'Music Resource',
-      description: 'A site of tutorials where you can learn about music.',
+      url: 'https://www.starwars.com/databank/darth-vader',
+      title: 'About me',
+      description: 'Once a heroic Jedi Knight, Darth Vader was seduced by the dark side of the Force.',
       user_id: 3,
-      topic_id: 1,
+      topic_id: 6,
       average_rating: 3.5
     }),
     knex('resources').insert({
       id: 2,
-      url: 'http://www.google.com',
-      title: 'Health and Wellness blog',
-      description: 'A blog about health and wellness',
+      url: 'https://www.maralagoclub.com/',
+      title: 'Mar-a-Lago fee',
+      description: 'My private resort membership fee doubled to $200,000 since I became president, hahahaha',
       user_id: 1,
-      topic_id: 4,
+      topic_id: 6,
       average_rating: 5
     }),
     knex('resources').insert({
       id: 3,
-      url: 'http://www.ign.com',
-      title: 'Writer\'s Blog',
-      description: 'A blog all about writing.',
+      url: 'https://vignette.wikia.nocookie.net/familyguy/images/8/8b/FGuy_Brian_Gen2012_R3Flat.jpg/revision/latest?cb=20120905115633',
+      title: 'I hate Brian',
+      description: 'Brian is a failure as a father and never sees his son',
       user_id: 2,
       topic_id: 2,
       average_rating: 4.5
     }),
     knex('resources').insert({
       id: 4,
-      url: 'http://www.gamespot.com',
-      title: 'Engineering 101',
-      description: 'A video about the fundamentals of engineering.',
+      url: 'https://i.ytimg.com/vi/80wqyrjIXQc/hqdefault.jpg',
+      title: 'Fake news',
+      description: 'I found a fake news website',
       user_id: 1,
       topic_id: 3,
-      average_rating: 1.5
+      average_rating: 3
     }),
     knex.raw('ALTER SEQUENCE resources_id_seq RESTART WITH 5')
   ]);
